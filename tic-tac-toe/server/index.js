779445716ea408fa0518a9f25567ebbe7d3d9867
pegaatron <ppt003@ucsd.edu>
2022-08-24 16:21:45 -0700

    initialize app, create server and client folders

---
+++ tic-tac-toe/server/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const db = require('./database/db.js');
+// const path = require('path')
+const controller = require('./controller');
+var cors = require('cors')
